858a53f552b79c72bc296443b6fd2e4e2b88bd4d
tohrnii <100405913+tohrnii@users.noreply.github.com>
2023-03-20 11:57:26 +0000

    test: add ir tests for selectors

--- ir/src/tests/selectors.rs
+++ ir/src/tests/selectors.rs
@@ -1,1 +1,67 @@
+use super::{parse, AirIR};
 
+#[ignore]
+#[test]
+fn single_selector() {
+    let source = "
+    trace_columns:
+        main: [s[2], clk]
+    
+    public_inputs:
+        stack_inputs: [16]
+    
+    boundary_constraints:
+        enf clk.first = 0
+    
+    integrity_constraints:
+        enf clk' = clk when s[0]";
+
+    let parsed = parse(source).expect("Parsing failed");
+    let result = AirIR::new(parsed);
+    assert!(result.is_ok());
+}
+
+#[ignore]
+#[test]
+fn chained_selectors() {
+    let source = "
+    trace_columns:
+        main: [s[3], clk]
+    
+    public_inputs:
+        stack_inputs: [16]
+    
+    boundary_constraints:
+        enf clk.first = 0
+    
+    integrity_constraints:
+        enf clk' = clk when (s[0] & !s[1]) | !s[2]'";
+
+    let parsed = parse(source).expect("Parsing failed");
+    let result = AirIR::new(parsed);
+    assert!(result.is_ok());
+}
+
+#[ignore]
+#[test]
+fn multiconstraint_selectors() {
+    let source = "
+    trace_columns:
+        main: [s[3], clk]
+    
+    public_inputs:
+        stack_inputs: [16]
+    
+    boundary_constraints:
+        enf clk.first = 0
+    
+    integrity_constraints:
+        enf clk' = 0 when s[0] & !s[1]
+        match enf:
+            clk' = clk when s[0] & s[1]
+            clk' = 1 when !s[0] & !s[1]";
+
+    let parsed = parse(source).expect("Parsing failed");
+    let result = AirIR::new(parsed);
+    assert!(result.is_ok());
+}
