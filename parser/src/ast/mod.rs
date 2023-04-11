1c3c8e3542660344144107b4987b7ad3c542c012
grjte <grjte@protonmail.com>
2023-04-11 10:13:55 +0100

    chore: minor docs fix & reordering for readability

--- parser/src/ast/mod.rs
+++ parser/src/ast/mod.rs
@@ -5,23 +5,25 @@ pub(crate) use air_script_core::{
     VectorAccess,
 };
 
-pub mod pub_inputs;
-pub use pub_inputs::PublicInput;
+// declaration modules
+pub mod evaluator_function;
+pub use evaluator_function::*;
 
 pub mod periodic_columns;
 pub use periodic_columns::PeriodicColumn;
 
+pub mod pub_inputs;
+pub use pub_inputs::PublicInput;
+
+pub mod random_values;
+pub use random_values::*;
+
+// constraint modules
 pub mod boundary_constraints;
 pub use boundary_constraints::*;
 
 pub mod integrity_constraints;
 pub use integrity_constraints::*;
-
-pub mod random_values;
-pub use random_values::*;
-
-pub mod evaluator_function;
-pub use evaluator_function::*;
 
 // AST
 // ================================================================================================
@@ -30,18 +32,26 @@ pub use evaluator_function::*;
 #[derive(Debug, Eq, PartialEq)]
 pub struct Source(pub Vec<SourceSection>);
 
-/// Source is divided into SourceSections.
-/// There are 6 types of Source Sections:
+/// Source is divided into SourceSections. Each source section is responsible for declarations of a
+/// specific type or for defining constraints of a specific type.
 /// - AirDef: Name of the air constraints module.
-/// - TraceCols: Trace Columns representing columns of the execution trace.
-/// - PublicInputs: Public inputs are each represented by a fixed-size array. At least one public
-///   input is required, but there is no limit to the number of public inputs that can be specified.
+///
+/// The type declaration sections are:
+/// - Constant: A constant is represented by a name and a value. Each [Constant] source section
+///   declares a single constant.
+/// - EvaluatorFunction: Evaluator functions take descriptions of the main and auxiliary traces as
+///   input, and enforce integrity constraints on those trace columns. Each [EvaluatorFunction]
+///   source section declares a single evaluator function
 /// - PeriodicColumns: Periodic columns are each represented by a fixed-size array with all of its
 ///   elements specified. The array length is expected to be a power of 2, but this is not checked
 ///   during parsing.
+/// - PublicInputs: Public inputs are each represented by a fixed-size array. At least one public
+///   input is required, but there is no limit to the number of public inputs that can be specified.
 /// - RandomValues: Random Values represent the randomness sent by the Verifier.
-/// - EvaluatorFunction: Evaluator Functions take descriptions of the main and auxiliary traces as
-///   input, and enforce integrity constraints on those trace columns.
+/// - Trace: A vector of trace segments, each containing a vector of trace bindings, which bind an
+///   identifier to one or more columns in the execution trace.
+///
+/// The constraint definition sections are:
 /// - BoundaryConstraints: Boundary Constraints to be enforced on the boundaries of columns defined
 ///   in the TraceCols section. Currently there are two types of boundaries, First and Last
 ///   representing the first and last rows of the column.
@@ -49,13 +59,18 @@ pub struct Source(pub Vec<SourceSection>);
 ///   in the TraceCols section.
 #[derive(Debug, Eq, PartialEq)]
 pub enum SourceSection {
+    // AIR name definition
     AirDef(Identifier),
+
+    // type declarations
     Constant(Constant),
-    Trace(Vec<Vec<TraceBinding>>),
-    PublicInputs(Vec<PublicInput>),
-    PeriodicColumns(Vec<PeriodicColumn>),
-    RandomValues(RandomValues),
     EvaluatorFunction(EvaluatorFunction),
+    PeriodicColumns(Vec<PeriodicColumn>),
+    PublicInputs(Vec<PublicInput>),
+    RandomValues(RandomValues),
+    Trace(Vec<Vec<TraceBinding>>),
+
+    // constraint definitions
     BoundaryConstraints(Vec<BoundaryStmt>),
     IntegrityConstraints(Vec<IntegrityStmt>),
 }
