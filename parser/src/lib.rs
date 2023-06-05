59f51422bd4a9909c398abc13ffc231c60acf794
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2023-06-05 17:27:54 -0400

    feat: add parse_file and parse_str helpers to parser api

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -34,6 +34,43 @@ pub fn parse(
     }
 }
 
+/// Parses the provided source and returns the AST.
+pub fn parse_file<P: AsRef<Path>>(
+    diagnostics: &DiagnosticsHandler,
+    codemap: Arc<CodeMap>,
+    source: P,
+) -> Result<ast::Program, ParseError> {
+    let parser = Parser::new((), codemap);
+    match parser.parse_file::<ast::Program, _, _>(diagnostics, source) {
+        Ok(ast) => Ok(ast),
+        Err(ParseError::Lexer(err)) => {
+            diagnostics.emit(err);
+            Err(ParseError::Failed)
+        }
+        Err(err) => Err(err),
+    }
+}
+
+/// Parses the provided source string with a default [CodeMap] and [DiagnosticsHandler].
+///
+/// This is primarily provided for use in tests, you should generally prefer [parse]
+pub fn parse_str(source: &str) -> Result<ast::Program, ParseError> {
+    use miden_diagnostics::{
+        term::termcolor::ColorChoice, DefaultEmitter, DiagnosticsConfig, Verbosity,
+    };
+
+    let codemap = Arc::new(CodeMap::new());
+    let emitter = Arc::new(DefaultEmitter::new(ColorChoice::Auto));
+    let config = DiagnosticsConfig {
+        verbosity: Verbosity::Warning,
+        warnings_as_errors: true,
+        no_warn: false,
+        display: Default::default(),
+    };
+    let diagnostics = DiagnosticsHandler::new(config, codemap.clone(), emitter);
+    parse(&diagnostics, codemap, source)
+}
+
 /// Parses a [Module] from the given path.
 ///
 /// This is primarily intended for use in the import resolution phase.
