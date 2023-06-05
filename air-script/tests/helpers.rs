8ca34769d2f979f82b29f9a098beb994b5aef066
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2023-06-05 17:34:52 -0400

    chore: update air-script cli

--- air-script/tests/helpers.rs
+++ air-script/tests/helpers.rs
@@ -1,75 +1,37 @@
-use codegen_winter::CodeGenerator;
-use ir::AirIR;
-use std::fs;
+use std::sync::Arc;
 
-#[derive(Debug)]
-pub enum TestError {
-    IO(String),
-    Parse(String),
-    IR(String),
-}
+use air_ir::CompileError;
+use air_pass::Pass;
+use codegen_winter::CodeGenerator;
+use miden_diagnostics::{
+    term::termcolor::ColorChoice, CodeMap, DefaultEmitter, DiagnosticsHandler,
+};
 
 pub struct Test {
     input_path: String,
 }
-
 impl Test {
     pub fn new(input_path: String) -> Self {
         Test { input_path }
     }
 
-    pub fn transpile(&self) -> Result<String, TestError> {
-        // load source input from file
-        let source = fs::read_to_string(&self.input_path).map_err(|err| {
-            TestError::IO(format!(
-                "Failed to open input file `{:?}` - {}",
-                self.input_path, err
-            ))
-        })?;
+    pub fn transpile(&self) -> Result<String, CompileError> {
+        let codemap = Arc::new(CodeMap::new());
+        let emitter = Arc::new(DefaultEmitter::new(ColorChoice::Auto));
+        let diagnostics = DiagnosticsHandler::new(Default::default(), codemap.clone(), emitter);
 
-        // parse the input file to the internal representation
-        let parsed = parse(source.as_str()).map_err(|_| {
-            TestError::Parse(format!(
-                "Failed to parse the input air file at {}",
-                &self.input_path
-            ))
-        })?;
-
-        let ir = AirIR::new(parsed).map_err(|_| {
-            TestError::IR(format!(
-                "Failed to convert the input air file at {} to IR representation",
-                &self.input_path
-            ))
-        })?;
+        // Parse from file to internal representation
+        let air = air_parser::parse_file(&diagnostics, codemap, &self.input_path)
+            .map_err(CompileError::Parse)
+            .and_then(|ast| {
+                let mut pipeline = air_parser::transforms::ConstantPropagation::new(&diagnostics)
+                    .chain(air_parser::transforms::Inlining::new(&diagnostics))
+                    .chain(air_ir::passes::AstToAir::new(&diagnostics));
+                pipeline.run(ast)
+            })?;
 
         // generate Rust code targeting Winterfell
-        let codegen = CodeGenerator::new(&ir);
+        let codegen = CodeGenerator::new(&air);
         Ok(codegen.generate())
-    }
-}
-
-/// Parses the provided source and returns the AST.
-fn parse(source: &str) -> Result<parser::ast::Source, parser::ParseError> {
-    use miden_diagnostics::{term::termcolor::ColorChoice, *};
-    use parser::{ast, ParseError, Parser};
-    use std::sync::Arc;
-
-    let codemap = Arc::new(CodeMap::new());
-    let emitter = Arc::new(DefaultEmitter::new(ColorChoice::Auto));
-    let config = DiagnosticsConfig {
-        verbosity: Verbosity::Warning,
-        warnings_as_errors: true,
-        no_warn: false,
-        display: Default::default(),
-    };
-    let diagnostics = DiagnosticsHandler::new(config, codemap.clone(), emitter);
-    let parser = Parser::new((), codemap);
-    match parser.parse_string::<ast::Source, _, _>(&diagnostics, source) {
-        Ok(ast) => Ok(ast),
-        Err(ParseError::Lexer(err)) => {
-            diagnostics.emit(err);
-            Err(ParseError::Failed)
-        }
-        Err(err) => Err(err),
     }
 }
