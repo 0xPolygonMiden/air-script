ed94097b6b0e6216167bd0252765eeed20e548a5
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2023-06-05 17:34:38 -0400

    chore: update codegen/winterfell

--- codegen/winterfell/src/lib.rs
+++ codegen/winterfell/src/lib.rs
@@ -1,11 +1,8 @@
+use air_ir::Air;
 use codegen::{Impl, Scope};
-use ir::AirIR;
-
-mod imports;
-use imports::add_imports;
 
 mod air;
-use air::add_air;
+mod imports;
 
 // GENERATE RUST CODE FOR WINTERFELL AIR
 // ================================================================================================
@@ -13,30 +10,19 @@ use air::add_air;
 /// CodeGenerator is used to generate a Rust implementation of the Winterfell STARK prover library's
 /// Air trait. The generated Air expresses the constraints specified by the AirIR used to build the
 /// CodeGenerator.
-pub struct CodeGenerator {
-    scope: Scope,
-}
+pub struct CodeGenerator;
+impl air_ir::CodeGenerator for CodeGenerator {
+    type Output = String;
 
-impl CodeGenerator {
-    // --- CONSTRUCTOR ----------------------------------------------------------------------------
-
-    /// Builds a new Rust scope that represents a Winterfell Air trait implementation for the
-    /// provided AirIR.
-    pub fn new(ir: &AirIR) -> Self {
+    fn generate(&self, ir: &Air) -> anyhow::Result<Self::Output> {
         let mut scope = Scope::new();
 
         // add winterfell imports.
-        add_imports(&mut scope);
+        imports::add_imports(&mut scope);
 
         // add an Air struct and Winterfell Air trait implementation for the provided AirIR.
-        add_air(&mut scope, ir);
+        air::add_air(&mut scope, ir);
 
-        Self { scope }
-    }
-
-    /// Returns a string of Rust code containing a Winterfell Air implementation for the AirIR with
-    /// which this [CodeGenerator] was instantiated.
-    pub fn generate(&self) -> String {
-        self.scope.to_string()
+        Ok(scope.to_string())
     }
 }
