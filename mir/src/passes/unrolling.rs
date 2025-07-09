b69d395f020ef6f352531de9cb6c87cf3dfef127
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-07-09 15:10:58 +0300

    Update unrolling.rs

--- mir/src/passes/unrolling.rs
+++ mir/src/passes/unrolling.rs
@@ -123,7 +123,7 @@ impl Pass for Unrolling<'_> {
     }
 }
 
-// For the first pass of Unrolling, we use a tweeked version of the Visitor trait,
+// For the first pass of Unrolling, we use a tweaked version of the Visitor trait,
 // each visit_*_bis function returns an Option<Link<Op>> instead of Result<(), CompileError>,
 // to mutate the nodes (e.g. modifying a Operation<Vectors> to Vector<Operations>)
 impl UnrollingFirstPass<'_> {
