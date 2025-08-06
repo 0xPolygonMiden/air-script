4e85bcaff04652aaa146fde3bc1cc8d1f372c069
Leo-Besancon <lb@massa.net>
2025-08-06 17:28:00 +0200

    feat: implement constant propagation pass for binary ops except Sub

--- mir/src/passes/constant_propagation.rs
+++ mir/src/passes/constant_propagation.rs
@@ -1,17 +1,13 @@
+use std::{collections::HashMap, ops::Deref};
+
 use air_pass::Pass;
-use miden_diagnostics::DiagnosticsHandler;
+use miden_diagnostics::{DiagnosticsHandler, SourceSpan, Spanned};
 
 use super::visitor::Visitor;
 use crate::{
-    CompileError,
-    ir::{Link, Mir, Node},
+    ir::{BackLink, ConstantValue, Graph, Link, Mir, MirValue, Node, Op, SpannedMirValue, Value}, passes::duplicate_node, CompileError
 };
 
-/// TODO MIR:
-/// If needed, implement constant propagation / folding pass on MIR
-/// Run through every operation in the graph
-/// If we can deduce the resulting value based on the constants of the operands,
-/// replace the operation itself with a constant
 pub struct ConstantPropagation<'a> {
     #[allow(unused)]
     diagnostics: &'a DiagnosticsHandler,
@@ -30,9 +26,174 @@ impl Pass for ConstantPropagation<'_> {
 }
 
 impl<'a> ConstantPropagation<'a> {
-    #[allow(unused)]
     pub fn new(diagnostics: &'a DiagnosticsHandler) -> Self {
         Self { diagnostics, work_stack: vec![] }
+    }
+}
+
+/// Helper function to fold constant binary operations (Add, Sub, Mul, Exp) into their
+/// resulting value if both operands are constant values.
+fn fold_binary_op(
+    lhs: Link<Op>,
+    rhs: Link<Op>,
+    parent: Link<Op>,
+    span: SourceSpan,
+) -> Result<Option<Link<Op>>, CompileError> {
+    let mut updated_binary_op = None;
+
+    let zero_mir_value = MirValue::Constant(ConstantValue::Felt(0));
+    let one_mir_value = MirValue::Constant(ConstantValue::Felt(1));
+
+    let zero_node = Value::create(SpannedMirValue {
+        value: zero_mir_value.clone(),
+        span,
+    });
+    let one_node = Value::create(SpannedMirValue {
+        value: one_mir_value.clone(),
+        span,
+    });
+
+    match (lhs.borrow().deref(), rhs.borrow().deref()) {
+        (
+            Op::Value(Value {
+                value:
+                    SpannedMirValue {
+                        value: MirValue::Constant(ConstantValue::Felt(lhs_const)),
+                        ..
+                    },
+                ..
+            }),
+            Op::Value(Value {
+                value:
+                    SpannedMirValue {
+                        value: MirValue::Constant(ConstantValue::Felt(rhs_const)),
+                        ..
+                    },
+                ..
+            }),
+        ) => {
+            let folded = match parent.borrow().deref() {
+                Op::Add(_) => lhs_const.checked_add(*rhs_const),
+                Op::Sub(_) => lhs_const.checked_sub(*rhs_const),
+                Op::Mul(_) => lhs_const.checked_mul(*rhs_const),
+                Op::Exp(_) => {
+                    let rhs_const = (*rhs_const).try_into().map_err(|_| CompileError::Failed)?;
+                    lhs_const.checked_pow(rhs_const)
+                },
+                _ => unreachable!("Unexpected parent operation: {:?}", parent),
+            };
+            if let Some(folded) = folded {
+                let new_value = Value::create(SpannedMirValue {
+                    value: MirValue::Constant(crate::ir::ConstantValue::Felt(folded)),
+                    span,
+                });
+                updated_binary_op = Some(new_value);
+            }
+        },
+        (Op::Value(Value { value: SpannedMirValue { value, .. }, .. }), _)
+            if *value == zero_mir_value =>
+        {
+            match parent.borrow().deref() {
+                Op::Add(_) => updated_binary_op = Some(duplicate_node(rhs.clone(), &mut HashMap::new())),
+                Op::Mul(_) | Op::Exp(_) => {
+                    updated_binary_op = Some(zero_node);
+                },
+                Op::Sub(_) => {},
+                _ => unreachable!("Unexpected parent operation: {:?}", parent),
+            }
+        },
+        (_, Op::Value(Value { value: SpannedMirValue { value, .. }, .. }))
+            if *value == zero_mir_value =>
+        {
+            match parent.borrow().deref() {
+                Op::Add(_) => updated_binary_op = Some(duplicate_node(lhs.clone(), &mut HashMap::new())),
+                Op::Sub(_) => {},
+                // FIXME: Sub with zero is a no-op, but we need it for Enf(Sub(x, 0)) to represent the constraint x = 0
+                //Op::Add(_) | Op::Sub(_) => updated_binary_op = Some(duplicate_node(lhs.clone(), &mut HashMap::new())),
+                Op::Mul(_) => {
+                    updated_binary_op = Some(zero_node);
+                },
+                Op::Exp(_) => {
+                    updated_binary_op = Some(one_node);
+                },
+                _ => unreachable!("Unexpected parent operation: {:?}", parent),
+            }
+        },
+        (other, Op::Value(Value { value: SpannedMirValue { value, .. }, .. }))
+        | (Op::Value(Value { value: SpannedMirValue { value, .. }, .. }), other)
+            if *value == one_mir_value =>
+        {
+            match parent.borrow().deref() {
+                Op::Add(_) | Op::Sub(_) | Op::Exp(_) => {},
+                Op::Mul(_) => {
+                    updated_binary_op = Some(duplicate_node(other.clone().into(), &mut HashMap::new()));
+                },
+                _ => unreachable!("Unexpected parent operation: {:?}", parent),
+            }
+        },
+        _ => {
+            // If either operand is not a constant, we cannot fold the operation
+            return Ok(None);
+        },
+    }
+
+    Ok(updated_binary_op)
+}
+
+// For the ConstantPropagation, we use a tweaked version of the Visitor trait,
+// each visit_*_bis function returns an Option<Link<Op>> instead of Result<(), CompileError>,
+// to mutate the nodes (e.g. modifying a Add(lhs, rhs) to Value(lhs + rhs)).
+impl ConstantPropagation<'_> {
+    fn visit_add_bis(
+        &mut self,
+        _graph: &mut Graph,
+        add: Link<Op>,
+    ) -> Result<Option<Link<Op>>, CompileError> {
+        // safe to unwrap because we just dispatched on it
+        let add_ref = add.as_add().unwrap();
+        let lhs = add_ref.lhs.clone();
+        let rhs = add_ref.rhs.clone();
+
+        fold_binary_op(lhs, rhs, add.clone(), add_ref.span())
+    }
+
+    fn visit_sub_bis(
+        &mut self,
+        _graph: &mut Graph,
+        sub: Link<Op>,
+    ) -> Result<Option<Link<Op>>, CompileError> {
+        // safe to unwrap because we just dispatched on it
+        let sub_ref = sub.as_sub().unwrap();
+        let lhs = sub_ref.lhs.clone();
+        let rhs = sub_ref.rhs.clone();
+
+        fold_binary_op(lhs, rhs, sub.clone(), sub_ref.span())
+    }
+
+    fn visit_mul_bis(
+        &mut self,
+        _graph: &mut Graph,
+        mul: Link<Op>,
+    ) -> Result<Option<Link<Op>>, CompileError> {
+        // safe to unwrap because we just dispatched on it
+        let mul_ref = mul.as_mul().unwrap();
+        let lhs = mul_ref.lhs.clone();
+        let rhs = mul_ref.rhs.clone();
+
+        fold_binary_op(lhs, rhs, mul.clone(), mul_ref.span())
+    }
+
+    fn visit_exp_bis(
+        &mut self,
+        _graph: &mut Graph,
+        exp: Link<Op>,
+    ) -> Result<Option<Link<Op>>, CompileError> {
+        // safe to unwrap because we just dispatched on it
+        let exp_ref = exp.as_exp().unwrap();
+        let lhs = exp_ref.lhs.clone();
+        let rhs = exp_ref.rhs.clone();
+
+        fold_binary_op(lhs, rhs, exp.clone(), exp_ref.span())
     }
 }
 
@@ -40,17 +201,86 @@ impl Visitor for ConstantPropagation<'_> {
     fn work_stack(&mut self) -> &mut Vec<Link<Node>> {
         &mut self.work_stack
     }
-    fn root_nodes_to_visit(
-        &self,
-        graph: &crate::ir::Graph,
-    ) -> Vec<crate::ir::Link<crate::ir::Node>> {
+
+    // We visit all boundary constraints and all integrity constraints
+    // No need to visit the functions or evaluators, as they should have been inlined before this
+    // pass
+    fn root_nodes_to_visit(&self, graph: &Graph) -> Vec<Link<Node>> {
         let boundary_constraints_roots_ref = graph.boundary_constraints_roots.borrow();
         let integrity_constraints_roots_ref = graph.integrity_constraints_roots.borrow();
+        let bus_roots: Vec<_> = graph
+            .buses
+            .values()
+            .flat_map(|b| b.borrow().clone().columns.into_iter().collect::<Vec<_>>())
+            .collect();
         let combined_roots = boundary_constraints_roots_ref
             .clone()
             .into_iter()
             .map(|bc| bc.as_node())
-            .chain(integrity_constraints_roots_ref.clone().into_iter().map(|ic| ic.as_node()));
+            .chain(integrity_constraints_roots_ref.clone().into_iter().map(|ic| ic.as_node()))
+            .chain(bus_roots.into_iter().map(|b| b.as_node()));
         combined_roots.collect()
+    }
+
+    fn visit_node(&mut self, graph: &mut Graph, node: Link<Node>) -> Result<(), CompileError> {
+        // In this pass, we both need to dispatch the visitor depending on the node type,
+        // and also mutate the node if needed. We implement custom visit_*_bis methods
+        // that returns a Some(updated_node) if we need to update the node's value.
+        let updated_op: Result<Option<Link<Op>>, CompileError> = match node.borrow().deref() {
+            Node::Add(a) => to_link_and(a.clone(), graph, |g, el| self.visit_add_bis(g, el)),
+            Node::Sub(s) => to_link_and(s.clone(), graph, |g, el| self.visit_sub_bis(g, el)),
+            Node::Mul(m) => to_link_and(m.clone(), graph, |g, el| self.visit_mul_bis(g, el)),
+            Node::Exp(e) => to_link_and(e.clone(), graph, |g, el| self.visit_exp_bis(g, el)),
+            // For all the following cases, there is nothing to fold
+            Node::Vector(_)
+            | Node::Matrix(_)
+            | Node::Enf(_)
+            | Node::Boundary(_)
+            | Node::BusOp(_)
+            | Node::Value(_)
+            | Node::Accessor(_)
+            | Node::None(_) => Ok(None),
+            Node::Function(_) | Node::Evaluator(_) | Node::Call(_) => {
+                unreachable!(
+                    "Unexpected node during Mir's ConstantPropagation: Function, Evaluators and Calls should have been inlined before this pass. Found: {:?}",
+                    node
+                );
+            },
+            Node::If(_) | Node::For(_) | Node::Fold(_) | Node::Parameter(_) => {
+                unreachable!(
+                    "Unexpected node during Mir's ConstantPropagation: If, For, Fold and Parameter should have been unrolled before this pass. Found: {:?}",
+                    node
+                );
+            },
+        };
+
+        // We update the node if needed
+        if let Some(updated_op) = updated_op? {
+            //println!("Updating node: {}", node.debug());
+            //println!("  With: {:#?}", updated_op);
+            node.as_op().unwrap().set(&updated_op);
+        }
+
+        Ok(())
+    }
+}
+
+// HELPERS FUNCTIONS
+// ================================================================================================
+
+/// Tries to upgrade a BackLink to a Link<Op> and apply a given closure to it if it is successful,
+/// otherwise returns None.
+fn to_link_and<F>(
+    back: BackLink<Op>,
+    graph: &mut Graph,
+    f: F,
+) -> Result<Option<Link<Op>>, CompileError>
+where
+    F: FnOnce(&mut Graph, Link<Op>) -> Result<Option<Link<Op>>, CompileError>,
+{
+    if let Some(op) = back.to_link() {
+        f(graph, op)
+    } else {
+        Ok(None)
     }
 }
