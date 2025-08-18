2b4d19ea01646d544777cdb13d87cef035df7bd9
Bobbin Threadbare <bobbinth@protonmail.com>
2025-08-17 21:45:16 -0700

    chore: minor CI changes

--- scripts/check-changelog.sh
+++ scripts/check-changelog.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
-set -u
+#!/bin/bash
+set -uo pipefail
 
-CHANGELOG_FILE="${1:-CHANGELOG.md}"
+CHANGELOG_FILE="CHANGELOG.md"
 
 if [ "${NO_CHANGELOG_LABEL}" = "true" ]; then
     # 'no changelog' set, so finish successfully
@@ -13,9 +13,9 @@ else
     if git diff --exit-code "origin/${BASE_REF}" -- "${CHANGELOG_FILE}"; then
         >&2 echo "Changes should come with an entry in the \"CHANGELOG.md\" file. This behavior
 can be overridden by using the \"no changelog\" label, which is used for changes
-that are trivial / explicitely stated not to require a changelog entry."
+that are trivial / explicitly stated not to require a changelog entry."
         exit 1
     fi
 
-    echo "The \"CHANGELOG.md\" file has been updated."
+    echo "The ${CHANGELOG_FILE} file has been updated."
 fi
