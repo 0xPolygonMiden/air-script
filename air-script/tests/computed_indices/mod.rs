6634e612b89748eda892e9e157e216db153b38f8
Leo-Besancon <lb@massa.net>
2025-08-18 15:09:46 +0200

    tests: add computed_indices_complex E2E test
    Fails for now

--- air-script/tests/computed_indices/mod.rs
+++ air-script/tests/computed_indices/mod.rs
@@ -1,4 +1,7 @@
 #[rustfmt::skip]
 #[allow(clippy::all)]
-mod computed_indices;
+mod computed_indices_complex;
+#[rustfmt::skip]
+#[allow(clippy::all)]
+mod computed_indices_simple;
 mod test_air;
--- air-script/tests/computed_indices/test_air.rs
+++ air-script/tests/computed_indices/test_air.rs
@@ -3,7 +3,7 @@ use winter_math::fields::f64::BaseElement as Felt;
 use winterfell::{Trace, TraceTable};
 
 use crate::{
-    computed_indices::computed_indices::{ComputedIndicesAir, PublicInputs},
+    computed_indices::computed_indices_simple::{ComputedIndicesAir, PublicInputs},
     helpers::{AirTester, MyTraceTable},
 };
 
