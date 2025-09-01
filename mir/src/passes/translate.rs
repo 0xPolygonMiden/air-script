19a68bc5da07b1caf242d4ed04428ab28c94b137
Leo-Besancon <lb@massa.net>
2025-09-01 10:26:09 +0200

    chore(mir): add comments to translate_let

--- mir/src/passes/translate.rs
+++ mir/src/passes/translate.rs
@@ -61,6 +61,9 @@ pub struct MirBuilder<'a> {
     mir: Mir,
     trace_columns: &'a Vec<ast::TraceSegment>,
     bindings: LexicalScope<&'a ast::Identifier, Link<Op>>,
+    // The root node is either the evaluator or function we're currently translating the body of,
+    // or None if we're not inside a function or evaluator (e.g. translating boundary / integrity
+    // constraints)
     root: Link<Root>,
     root_name: Option<&'a ast::QualifiedIdentifier>,
     in_boundary: bool,
@@ -374,6 +377,8 @@ impl<'a> MirBuilder<'a> {
         }
     }
 
+    /// Translates a statement and returns the operation.
+    /// Note: for `let` statements, the returned operation is the last operation of its body.
     fn translate_statement(&mut self, stmt: &'a ast::Statement) -> Result<Link<Op>, CompileError> {
         match stmt {
             ast::Statement::Let(let_stmt) => self.translate_let(let_stmt),
@@ -384,6 +389,15 @@ impl<'a> MirBuilder<'a> {
             ast::Statement::BusEnforce(list_comp) => self.translate_bus_enforce(list_comp),
         }
     }
+
+    /// Translates a let statement by:
+    /// - binding its value to its name for the scope of its body,
+    /// - adding the statements of its body to the root's body if necessary,
+    /// - returning the operation of the last statement of its body, which is the value of the whole
+    ///   block.
+    ///
+    /// Note: as we already return the operation of the last statement, we do not need to add it to
+    /// the root's body here. This should be handled by the caller.
     fn translate_let(&mut self, let_stmt: &'a ast::Let) -> Result<Link<Op>, CompileError> {
         let name = &let_stmt.name;
         let value: Link<Op> = self.translate_expr(&let_stmt.value)?;
@@ -392,10 +406,14 @@ impl<'a> MirBuilder<'a> {
         self.bindings.insert(name, value.clone());
         for (i, stmt) in let_stmt.body.iter().enumerate() {
             let new_stmt = self.translate_statement(stmt)?;
+            // Skip the last statement as it is returned
             if i < let_stmt.body.len() - 1 {
                 match self.root.borrow().deref() {
                     Root::Function(f) => f.body.borrow_mut().push(new_stmt.clone()),
                     Root::Evaluator(e) => e.body.borrow_mut().push(new_stmt.clone()),
+                    // Root::None means we are translating statements of boundary / integrity
+                    // constraints, for which statements are inserted on enforce, nothing to do
+                    // here.
                     Root::None(_span) => {},
                 }
             }
