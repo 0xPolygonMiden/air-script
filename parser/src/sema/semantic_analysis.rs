d5350d2ee3df62f526fce1cc9e3744d5113d21aa
al-kin <82364884+Al-Kindi-0@users.noreply.github.com>
2025-09-19 13:30:15 +0400

    feat: add ability to import functions

--- parser/src/sema/semantic_analysis.rs
+++ parser/src/sema/semantic_analysis.rs
@@ -1905,6 +1905,17 @@ impl SemanticAnalysis<'_> {
                         )
                     })
                 })
+                .or_else(|| {
+                    imported_from.functions.get(qid.as_ref()).map(|f| {
+                        Span::new(
+                            f.span(),
+                            BindingType::Function(FunctionType::Function(
+                                f.param_types(),
+                                f.return_type,
+                            )),
+                        )
+                    })
+                })
                 .ok_or(InvalidAccessError::UndefinedVariable)
         }
     }
