64bcc336777c40c3b61ae6d9be4caac6262da78e
Thybault Alabarbe <thybault.alabarbe@gmail.com>
2025-10-20 13:13:14 +0200

    tests: remove test for nested slice slicing

--- air-script/tests/codegen/winterfell.rs
+++ air-script/tests/codegen/winterfell.rs
@@ -172,15 +172,17 @@ fn evaluators_nested_slice_call() {
     expected.assert_eq(&generated_air);
 }
 
-#[test]
-fn evaluators_slice_slicing() {
-    let generated_air = Test::new("tests/evaluators/evaluators_slice_slicing.air".to_string())
-        .transpile(Target::Winterfell)
-        .unwrap();
-
-    let expected = expect_file!["../evaluators/evaluators_slice_slicing.rs"];
-    expected.assert_eq(&generated_air);
-}
+// TODO: add support for nested slicing in general expressions.
+//
+// #[test]
+// fn evaluators_slice_slicing() {
+//     let generated_air = Test::new("tests/evaluators/evaluators_slice_slicing.air".to_string())
+//         .transpile(Target::Winterfell)
+//         .unwrap();
+//
+//     let expected = expect_file!["../evaluators/evaluators_slice_slicing.rs"];
+//     expected.assert_eq(&generated_air);
+// }
 
 #[test]
 fn fibonacci() {
