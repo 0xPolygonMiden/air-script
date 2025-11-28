dc8043fdda4ee85885d4d6a6c4d6d4b9ff1bf39d
Leo-Besancon <lb@massa.net>
2025-11-28 10:31:40 +0100

    docs: add math formulas for buses_transitions

--- air/src/ir/mod.rs
+++ air/src/ir/mod.rs
@@ -199,7 +199,13 @@ pub struct Air {
     pub buses_initial_values: BTreeMap<usize, NodeIndex>,
     /// Buses transition expressions used for auxiliary trace generation (indexed by bus index)
     /// The tuple contains the numerator and an optional denominator operation (p_prime = numerator
-    /// if None or numerator / denominator)
+    /// if None or numerator / denominator), computed in the `ExpandBuses` Air pass depending on
+    /// the bus type
+    /// - for multiset buses: p' = p * columns_inserted_in_bus / columns_removed_from_bus
+    /// - for logup buses, if u corresponds to the columns inserted when s1 and v to the columns
+    ///   removed when s2
+    /// q' = q + s1 / u - s2 / v
+    ///    = (q * u * v + s1 * v - s2 * u) / (u * v)
     pub buses_transitions: BTreeMap<usize, (NodeIndex, Option<NodeIndex>)>,
 }
 impl Default for Air {
