16ac79f89e783285ae59c730d9ebcc58bde5c21c
Leo-Besancon <lb@massa.net>
2025-12-05 13:11:32 +0100

    refactor: move `miden_vm_aux_trace_generator` outside of tests for export

--- air-script/src/test_utils/mod.rs
+++ air-script/src/test_utils/mod.rs
@@ -2,7 +2,5 @@
 pub mod air_tester_macros;
 /// Code generation for tests/**/*.air files.
 pub mod codegen;
-/// Miden VM auxiliary trace generator
-pub mod miden_vm_aux_trace_generator;
 /// Winterfell-specific traits
 pub mod winterfell_traits;
