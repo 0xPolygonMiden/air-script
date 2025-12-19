b3cda7df9d90eb6b0f726735bad7e00f6a9e8b6c
Thybault Alabarbe <thybault.alabarbe@gmail.com>
2025-12-19 18:18:42 +0100

    docs(mir/inlining): remove wrong comment

--- mir/src/passes/inlining.rs
+++ mir/src/passes/inlining.rs
@@ -476,7 +476,6 @@ impl Visitor for InliningSecondPass<'_> {
                 self.visit_call(graph, call_op.clone())?;
                 // NOTE: Once the call has been processed, we return early to avoid
                 // an infinite loop.
-                // This can happen in some cases when you have nested calls accross
                 // modules.
                 // If there are nested calls, they will be handled via fixed-point
                 // compilation (see [<Inlining<'_> as Pass>::run]).
