fec85728982cd2495504635e67b6a7be8c4fbf29
Thybault Alabarbe <thybault.alabarbe@gmail.com>
2026-01-30 19:38:28 +0100

    tests(compare-wf-p3): variables test

--- air-script/src/tests/comparison/mod.rs
+++ air-script/src/tests/comparison/mod.rs
@@ -29,3 +29,4 @@ mod selectors_combine_with_list_comprehensions;
 mod selectors_with_evaluators;
 mod system;
 mod trace_col_groups;
+mod variables;
--- air-script/src/tests/variables/mod.rs
+++ air-script/src/tests/variables/mod.rs
@@ -2,8 +2,8 @@ mod test_air_plonky3;
 mod test_air_winterfell;
 #[rustfmt::skip]
 #[allow(clippy::all)]
-mod variables;
+pub mod variables;
 #[rustfmt::skip]
 #[allow(clippy::all)]
 #[allow(unused_imports)]
-mod variables_plonky3;
+pub mod variables_plonky3;
