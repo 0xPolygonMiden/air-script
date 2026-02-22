53d716e8b43c00220065ba4e2fda91903a4148b0
al <82364884+Al-Kindi-0@users.noreply.github.com>
2026-02-22 20:17:50 +0400

    fix: restore constraints manifest and bus metadata

--- air/src/ir/mod.rs
+++ air/src/ir/mod.rs
@@ -197,15 +197,17 @@ pub struct Air {
     ///
     /// Only their name, type, and the first and last boundary constraints are stored here.
     pub buses: BTreeMap<Identifier, Bus>,
-    /// Buses initial values used for auxiliary trace generation (indexed by bus index)
+    /// Buses initial values used for auxiliary trace generation (indexed by bus index).
     pub buses_initial_values: BTreeMap<usize, NodeIndex>,
-    /// Buses transition expressions used for auxiliary trace generation (indexed by bus index)
-    /// The tuple contains the numerator and an optional denominator operation (p_prime = numerator
-    /// if None or numerator / denominator), computed in the `ExpandBuses` Air pass depending on
-    /// the bus type
+    /// Buses transition expressions used for auxiliary trace generation (indexed by bus index).
+    /// The tuple contains the numerator and an optional denominator operation (p' = numerator if
+    /// None or numerator / denominator), computed in the `ExpandBuses` Air pass depending on the
+    /// bus type.
     /// - for multiset buses: p' = p * columns_inserted_in_bus / columns_removed_from_bus
     /// - for logup buses, if u corresponds to the columns inserted when s1 and v to the columns
-    ///   removed when s2: q' = q + s1 / u - s2 / v = (q * u * v + s1 * v - s2 * u) / (u * v)
+    ///   removed when s2:
+    ///   q' = q + s1 / u - s2 / v
+    ///   = (q * u * v + s1 * v - s2 * u) / (u * v)
     pub buses_transitions: BTreeMap<usize, (NodeIndex, Option<NodeIndex>)>,
 }
 impl Default for Air {
