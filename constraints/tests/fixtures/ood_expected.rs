c013bb3d7d10d0aeb944121d5a1cc808378019b9
al <82364884+Al-Kindi-0@users.noreply.github.com>
2026-02-27 17:20:17 +0400

    fix post rebase

--- constraints/tests/fixtures/ood_expected.rs
+++ constraints/tests/fixtures/ood_expected.rs
@@ -2266,7 +2266,7 @@ pub fn expected_ood_evals() -> Vec<EvalRecord> {
         EvalRecord {
             id: 373,
             name: "chiplets.bus.chiplets.transition",
-            value: QuadFelt::new(Felt::new(14696969614088113799), Felt::new(1398723567985082498)),
+            value: QuadFelt::new(Felt::new(7990980974626587792), Felt::new(5675027937982935418)),
         },
         EvalRecord {
             id: 374,
